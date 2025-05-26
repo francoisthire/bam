a38c2dffc54960f18aabe3917209b2840f3eb660
FrançoisT <franth2@gmail.com>
2025-05-26 11:53:50 +0200

    Fix typo smallar -> smaller

--- example/2-simple-failure/simple_failure.ml
+++ example/2-simple-failure/simple_failure.ml
@@ -3,7 +3,7 @@ open Tezt_bam
 let register () =
   let gen = Std.int () in
   let property x =
-    if x < 100 then Ok () else Error (`Fail "integer is not smallar than 100")
+    if x < 100 then Ok () else Error (`Fail "integer is not smaller than 100")
   in
   Pbt.register ~pp:Format.pp_print_int ~__FILE__
     ~title:"Simple failure example with bam" ~tags:["bam"; "simple_failure"]
